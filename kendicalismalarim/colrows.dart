ad431d7e7d510713198a42c111e6c2cab0901f58
cbesmhsyn96 <system.c.hsyn@gmail.com>
2020-09-09 22:20:48 +0300

    colrows güncellendi
    Column içinde Row dizisi tanımlandı

---
+++ kendicalismalarim/colrows.dart
@@ -0,0 +1,483 @@
+import 'package:flutter/material.dart';
+
+class colrows_sayfa extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      color: Colors.blue[200],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        textDirection: TextDirection.ltr,
+        verticalDirection: VerticalDirection.down,
+        textBaseline: TextBaseline.alphabetic,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            textDirection: TextDirection.ltr,
+            verticalDirection: VerticalDirection.down,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            textDirection: TextDirection.ltr,
+            verticalDirection: VerticalDirection.down,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            textDirection: TextDirection.ltr,
+            verticalDirection: VerticalDirection.down,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Text(
+                  "derdedadasd",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
