e1e9d69add94de775e480afdc8ce3f91ce879965
cbesmhsyn96 <system.c.hsyn@gmail.com>
2020-09-10 11:14:24 +0300

    Create colsrows100920.dart
    Satır ve sütun listesiyle ilgili son çalışma.

---
+++ kendicalismalarim/colsrows100920.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+
+class ClsRws extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.deepOrange,
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        textDirection: TextDirection.ltr,
+        verticalDirection: VerticalDirection.down,
+        textBaseline: TextBaseline.alphabetic,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            textDirection: TextDirection.ltr,
+            verticalDirection: VerticalDirection.down,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            textDirection: TextDirection.ltr,
+            verticalDirection: VerticalDirection.down,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            textDirection: TextDirection.ltr,
+            verticalDirection: VerticalDirection.down,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            textDirection: TextDirection.ltr,
+            verticalDirection: VerticalDirection.down,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            textDirection: TextDirection.ltr,
+            verticalDirection: VerticalDirection.down,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.black,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
