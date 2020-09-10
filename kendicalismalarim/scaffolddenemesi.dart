d8477b8d5ae9f1c575853515e4b7aad687cbdabd
cbesmhsyn96 <system.c.hsyn@gmail.com>
2020-09-10 18:44:54 +0300

    scaffold çalışmaları
    İçeriği
    RaisedButton
    Icon
    BottomNavigationBar
    AppBar
    IconButton
    -->Scaffold' da önemli nokta MaterialApp içinde olması. Bu yüzden farklı bir dosyada Scaffold döndürüldüğü zaman main.dart' ta runapp kısmında MaterialApp(dosyanın sınıf ismi) olarak çalıştırılır.

---
+++ kendicalismalarim/scaffolddenemesi.dart
@@ -0,0 +1,334 @@
+import 'package:flutter/material.dart';
+
+class Scf extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        centerTitle: true,
+        title: Text(
+          "Scaffold Denemesi",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.accessible),
+            onPressed: () => {print("Accessible")},
+            tooltip: "Acc",
+          ),
+          IconButton(
+            icon: Icon(Icons.account_balance),
+            onPressed: () => {print("AccountB")},
+            tooltip: "AccountB",
+          ),
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () => {print("Circle")},
+            tooltip: "Circle",
+          ),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        color: Colors.blue,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          textDirection: TextDirection.ltr,
+          verticalDirection: VerticalDirection.down,
+          textBaseline: TextBaseline.alphabetic,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              textDirection: TextDirection.ltr,
+              verticalDirection: VerticalDirection.down,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              textDirection: TextDirection.ltr,
+              verticalDirection: VerticalDirection.down,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              textDirection: TextDirection.ltr,
+              verticalDirection: VerticalDirection.down,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              textDirection: TextDirection.ltr,
+              verticalDirection: VerticalDirection.down,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              textDirection: TextDirection.ltr,
+              verticalDirection: VerticalDirection.down,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              textDirection: TextDirection.ltr,
+              verticalDirection: VerticalDirection.down,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              textDirection: TextDirection.ltr,
+              verticalDirection: VerticalDirection.down,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () => {print("Submit")},
+                    child: Text('Submit'),
+                    //other properties
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.accessibility),
+            title: Text("İcon"),
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.accessibility),
+            title: Text("İcon"),
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.accessibility),
+            title: Text("İcon"),
+            backgroundColor: Colors.amber,
+          ),
+        ],
+        onTap: (int i) => print("Index --> $i"),
+      ),
+    );
+  }
+}
+
+/*
+
+ */
---
+++ kendicalismalarim/scfoldcalisma.dart
@@ -0,0 +1,282 @@
+import 'package:flutter/material.dart';
+
+class Dene extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        title: Text("Scaffold Çalışması"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () => print("Hesap"),
+            color: Colors.white,
+          ),
+          IconButton(
+            icon: Icon(Icons.add_a_photo),
+            onPressed: () => print("Fotoğraf Ekle"),
+            color: Colors.white,
+          ),
+          IconButton(
+            icon: Icon(Icons.access_alarm),
+            onPressed: () => print("Alarm"),
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.grey,
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              textDirection: TextDirection.ltr,
+              verticalDirection: VerticalDirection.down,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  textDirection: TextDirection.ltr,
+                  verticalDirection: VerticalDirection.down,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  textDirection: TextDirection.ltr,
+                  verticalDirection: VerticalDirection.down,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  textDirection: TextDirection.ltr,
+                  verticalDirection: VerticalDirection.down,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  textDirection: TextDirection.ltr,
+                  verticalDirection: VerticalDirection.down,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  textDirection: TextDirection.ltr,
+                  verticalDirection: VerticalDirection.down,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ]),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_balance,
+              color: Colors.white,
+            ),
+            title: Text(
+              "Acc",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_balance,
+              color: Colors.white,
+            ),
+            title: Text(
+              "Acc",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_balance,
+              color: Colors.white,
+            ),
+            title: Text(
+              "Acc",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+        onTap: (int i) => print("Tıklanılan botonun indeksi $i"),
+      ),
+    );
+  }
+}
