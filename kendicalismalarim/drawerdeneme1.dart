b2200ab1a4babb1e897883519842f75b341f1ac0
cbesmhsyn96 <system.c.hsyn@gmail.com>
2020-09-11 17:19:57 +0300

    Create drawerdeneme1.dart
    Drawer ve GridList' in çalışma mantığına baktım.

---
+++ kendicalismalarim/drawerdeneme1.dart
@@ -0,0 +1,465 @@
+import 'package:flutter/material.dart';
+
+class Scfsinif extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drawer Demo'),
+      ),
+      body: GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 3,
+        // Generate 100 widgets that display their index in the List.
+        children: [
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              "Deneme",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
