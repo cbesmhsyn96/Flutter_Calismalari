02cb613daeb5a8cdd5a461a9b0605bd668ae7c01
cbesmhsyn96 <system.c.hsyn@gmail.com>
2020-09-14 11:01:26 +0300

    Create statefulkullanimi_1.dart

---
+++ kendicalismalarim/statefulkullanimi_1.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class Sayfa extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Stateful Çalışması",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int durum = 0;
+  arttir() {
+    setState(() {
+      durum++;
+    });
+  }
+
+  azalt() {
+    setState(() {
+      durum--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("AppBar Yazısı"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        color: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FlatButton(
+              onPressed: () => arttir(),
+              child: Text("Arttır"),
+            ),
+            Text("Sayı --> $durum"),
+            FlatButton(
+              onPressed: () => azalt(),
+              child: Text("Azalt"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
